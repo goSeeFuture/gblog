fed1b06e0787914c560d4b189a1b537450eabb8d
GodanKing <GodanKing@qq.com>
2021-04-22 21:54:42 +0800

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-APP="gblog"
+rm -f dist/gblog_linux_amd64  dist/gblog_windows_amd64.exe dist/*.zip
 
 packr2 
-gox -tags="embed" -osarch="linux/amd64" -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}"
-gox -tags="embed" -osarch="windows/amd64" -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}"
-     
+gox -tags="embed" -osarch="linux/amd64" -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}" && upx -9 dist/gblog_linux_amd64
+gox -tags="embed" -osarch="windows/amd64" -output="dist/{{.Dir}}_{{.OS}}_{{.Arch}}" && upx -9 dist/gblog_windows_amd64.exe
+
+cd dist
+
+zip -r gblog_linux_amd64.zip  gblog_linux_amd64 articles static config.toml
+zip -r gblog_windows_amd64.zip  gblog_windows_amd64.exe articles static config.toml
